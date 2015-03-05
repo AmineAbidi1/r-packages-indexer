ad20bd0dfa0cd3e682b01f3b78bbb0109be83a10
Amine Abidi <med.amine.labidi@gmail.com>
2015-03-05 11:53:42 +0100

    tests for controller

---
+++ spec/controllers/packages_controller_spec.rb
@@ -0,0 +1,20 @@
+#encoding:utf-8
+require 'rails_helper'
+
+describe PackagesController do
+	before do
+		
+	end
+	
+	describe '.index' do
+		it 'must list all packages' do
+			#pending
+		end
+	end
+
+	describe '.show' do
+		it 'must list a package by providing its id' do
+			#pending
+		end
+	end
+end
